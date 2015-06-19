09748ebf6c1164683897fcf5c1c2f62f35f97f1d
Alena_Laptseva <Alena_Laptseva@epam.com>
2015-06-19 18:17:40 +0300

    setup

--- main/GenDocGithub.py
+++ main/GenDocGithub.py
@@ -1,1 +1,19 @@
 __author__ = 'Alena_Laptseva'
+import cmd
+import git
+# from git import Repo
+from github import Github
+import github
+
+g = Github("pavelpantseleyeu", "")
+g.get_api_status()
+
+
+dir = git.cmd.Git("C:\Users\Pavel_Pantseleyeu\PycharmProjects\QAAutomation")
+dir.pull()
+
+for repo in g.get_user().get_repos():
+    print repo.name
+    repo.edit(has_wiki=False)
+
+repo = Repo('https://github.com/ClarabridgeInc
